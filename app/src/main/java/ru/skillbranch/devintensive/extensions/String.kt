044ba956ec5714e100f5b3aa2681b3ec27c980e0
Fradan <roumen14@yandex.ru>
2019-07-06 11:47:35 +0300

    Добавлены экстеншены для строки

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,19 @@
+package ru.skillbranch.devintensive.extensions
+
+/**
+ * Created by 580305 on 06.07.2019.
+ */
+
+fun String.truncate(amount : Int = 16) : String {
+    var tmp = this.trimEnd()
+    tmp = if (tmp.length > amount) "${tmp.substring(0, amount).trimEnd()}..." else tmp
+    return tmp
+}
+
+fun String.stripHtml() : String {
+    var regex = "<[^>]*>".toRegex()
+    var escapeSymbols = "[&<>'\"]+".toRegex()
+    var res = regex.replace(this, "")
+    res = escapeSymbols.replace(res, "")
+    return r.replace("  ", "")
+}
